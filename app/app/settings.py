32fe7a2a16b8f0e0dc789a9133dfa7f050aa9624
italo-carvalho <italocarvalhoti@hotmail.com>
2021-08-22 18:47:57 -0300

    Added endpoint for creating user

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,7 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e-r$0^vwqsf!il8po_*p^odp3a#_2_7nfe)@sns24y2slokgqm"
+SECRET_KEY = (
+    "django-insecure-e-r$0^vwqsf!il8po_*p^odp3a#_2_7nfe)@sns24y2slokgqm"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -26,7 +28,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
     "core",
+    "user",
 ]
 
 MIDDLEWARE = [
